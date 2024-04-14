c263f7af27ac4591c8e2d3724ea2a7898b49042d
Mathias <58486836+sueppchen@users.noreply.github.com>
2024-04-14 09:48:11 +0200

    changed table
    and crc geneeration

--- investigation/advanced.ino
+++ investigation/advanced.ino
@@ -90,88 +90,77 @@ uint8_t byteArray[4][9] = {
   // some values are missing in the next two tables, replaced by _NONE_ (0xFFFF)
   // cannot be used to calculate a valid CRC
   #define _NONE_ 0xFFFF
-  uint16_t table_M[] = {
-      0x0000, _NONE_, 0x3D30, _NONE_, _NONE_, _NONE_, _NONE_, _NONE_, 
-      _NONE_, _NONE_, _NONE_, _NONE_, _NONE_, _NONE_, _NONE_, _NONE_, 
-      _NONE_, _NONE_, _NONE_, _NONE_, _NONE_, _NONE_, _NONE_, _NONE_, 
-      _NONE_, _NONE_, _NONE_, _NONE_, _NONE_, _NONE_, _NONE_, _NONE_,  
-      _NONE_, _NONE_, _NONE_, _NONE_, _NONE_, _NONE_, _NONE_, _NONE_, 
-      _NONE_, _NONE_, _NONE_, _NONE_, _NONE_, _NONE_, _NONE_, _NONE_,  
-      _NONE_, _NONE_, _NONE_, _NONE_, _NONE_, _NONE_, _NONE_, _NONE_, 
-      _NONE_, _NONE_, _NONE_, _NONE_, _NONE_, _NONE_, _NONE_, _NONE_}; 
-
-  uint16_t table_G[] = { 
-      0x0000, 0x0f0f, 0x1129, 0x1b23, 0x2800, 0x050d, 0x0b32, 0x0a12, // 00 - 07
-      0x2d25, 0x3c14, 0x223a, 0x330b, 0x271f, 0x2808, 0x3901, 0x3c24, // 08 - 0F
-      0x153c, 0x1019, 0x1f16, 0x0128, 0x0138, 0x041d, 0x1a33, 0x0b02, // 10 - 17
-      0x331b, 0x363e, 0x0008, 0x2d05, 0x0e27, 0x143c, 0x1b33, 0x0a02, // 18 - 1F
-      0x1131, 0x0b2a, 0x1031, 0x1514, 0x0e0f, 0x1534, 0x0100, 0x0405, // 20 - 27
-      0x0425, 0x1009, 0x0e17, 0x152c, 0x1a1b, 0x0120, 0x1a3b, 0x1f1e, // 28 - 2F
-      0x0e07, 0x1f36, 0x3911, 0x2820, 0x3313, 0x2222, 0x222a, 0x3919, // 30 - 37
-      0x1e3e, 0x0505, 0x3636, 0x1b3b, 0x2d2d, 0x3616, 0x2d0d, 0x3333}; // 38 - 3f
-
-  uint16_t table_R[] = {
-      0x0000, 0x3a1b, 0x3f02, 0x0e2d, 0x0e2f, 0x020f, 0x0005, 0x0d25, 
+  uint16_t table[7][64] = {
+    { 0x0000, _NONE_, 0x3D30, _NONE_, _NONE_, _NONE_, _NONE_, _NONE_,  // MODE 00 - 07
+      _NONE_, _NONE_, _NONE_, _NONE_, _NONE_, _NONE_, _NONE_, _NONE_,  // MODE 08 - 0F
+      _NONE_, _NONE_, _NONE_, _NONE_, _NONE_, _NONE_, _NONE_, _NONE_,  // MODE 00 - 17
+      _NONE_, _NONE_, _NONE_, _NONE_, _NONE_, _NONE_, _NONE_, _NONE_,  // MODE 08 - 1F
+      _NONE_, _NONE_, _NONE_, _NONE_, _NONE_, _NONE_, _NONE_, _NONE_,  // MODE 00 - 27
+      _NONE_, _NONE_, _NONE_, _NONE_, _NONE_, _NONE_, _NONE_, _NONE_,  // MODE 08 - 2F
+      _NONE_, _NONE_, _NONE_, _NONE_, _NONE_, _NONE_, _NONE_, _NONE_,  // MODE 00 - 37
+      _NONE_, _NONE_, _NONE_, _NONE_, _NONE_, _NONE_, _NONE_, _NONE_}, // MODE 08 - 3F
+      //mode0: green, mode2: mem(end<<4 | start ), mode4:green1,
+    { 0x0000, 0x0f0f, 0x1129, 0x1b23, 0x2800, 0x050d, 0x0b32, 0x0a12,  // 00 - 07
+      0x2d25, 0x3c14, 0x223a, 0x330b, 0x271f, 0x2808, 0x3901, 0x3c24,  // 08 - 0F
+      0x153c, 0x1019, 0x1f16, 0x0128, 0x0138, 0x041d, 0x1a33, 0x0b02,  // 10 - 17
+      0x331b, 0x363e, 0x0008, 0x2d05, 0x0e27, 0x143c, 0x1b33, 0x0a02,  // 18 - 1F
+      0x1131, 0x0b2a, 0x1031, 0x1514, 0x0e0f, 0x1534, 0x0100, 0x0405,  // 20 - 27
+      0x0425, 0x1009, 0x0e17, 0x152c, 0x1a1b, 0x0120, 0x1a3b, 0x1f1e,  // 28 - 2F
+      0x0e07, 0x1f36, 0x3911, 0x2820, 0x3313, 0x2222, 0x222a, 0x3919,  // 30 - 37
+      0x1e3e, 0x0505, 0x3636, 0x1b3b, 0x2d2d, 0x3616, 0x2d0d, 0x3333}, // 38 - 3f
+    // mode0: red, mode2: attack+random, mode4: red1
+    { 0x0000, 0x3a1b, 0x3f02, 0x0e2d, 0x0e2f, 0x020f, 0x0005, 0x0d25, 
       0x083f, 0x0b37, 0x0715, 0x041d, 0x0105, 0x0714, 0x3532, 0x3322,
       0x0103, 0x0713, 0x3d08, 0x041b, 0x312a, 0x373a, 0x3b18, 0x3810, 
       0x312c, 0x373c, 0x093b, 0x051b, 0x3e00, 0x0107, 0x3b1c, 0x3814,
       0x0308, 0x3c0f, 0x030c, 0x051c, 0x3a1f, 0x0838, 0x0004, 0x0b30, 
       0x0614, 0x3222, 0x0615, 0x3432, 0x3f07, 0x0d20, 0x3223, 0x3433,
       0x3324, 0x302c, 0x0003, 0x030b, 0x3817, 0x3b1f, 0x3224, 0x0938, 
-      0x3913, 0x0b34, 0x3e07, 0x3227, 0x0104, 0x3323, 0x0c20, 0x3533};
-
-  uint16_t table_B[]  = {
-      0x0000, 0x3108, 0x1108, 0x0f07, 0x373b, 0x3a2f, 0x0b20, 0x0630, 
+      0x3913, 0x0b34, 0x3e07, 0x3227, 0x0104, 0x3323, 0x0c20, 0x3533},
+    // mode0: blue, mode2: release+hold, mode4: blue1
+    { 0x0000, 0x3108, 0x1108, 0x0f07, 0x373b, 0x3a2f, 0x0b20, 0x0630, 
       0x0212, 0x0b25, 0x3e0d, 0x373a, 0x0b24, 0x2213, 0x2935, 0x1c1c, 
       0x2426, 0x110f, 0x2007, 0x1838, 0x152f, 0x2006, 0x152e, 0x1c19, 
       0x3a2d, 0x0f04, 0x1528, 0x183c, 0x2930, 0x3318, 0x0210, 0x0b27, 
       0x0937, 0x131f, 0x1e09, 0x2b20, 0x2636, 0x310e, 0x173e, 0x3839, 
       0x2217, 0x1c18, 0x3e09, 0x2931, 0x1a28, 0x0d10, 0x0006, 0x352f, 
       0x331e, 0x3a29, 0x2422, 0x2d15, 0x2f05, 0x2632, 0x331a, 0x310a, 
-      0x383f, 0x2f07, 0x1a2c, 0x1738, 0x173a, 0x0002, 0x0d14, 0x352b};
-
-  uint16_t table_AR[] = {
-      0x0000, 0x2037, 0x2e38, 0x3320, 0x3c18, _NONE_, _NONE_, _NONE_, 
+      0x383f, 0x2f07, 0x1a2c, 0x1738, 0x173a, 0x0002, 0x0d14, 0x352b},
+    // mode0: attack+random, mode2: none, mode4: green2
+    { 0x0000, 0x2037, 0x2e38, 0x3320, 0x3c18, _NONE_, _NONE_, _NONE_, 
       0x0d05, 0x0628, 0x2b2b, 0x2006, 0x1a36, _NONE_, _NONE_, _NONE_, 
       0x2c39, 0x1d24, 0x3d13, 0x1608, 0x1010, _NONE_, _NONE_, _NONE_, 
       0x261f, 0x1702, 0x230c, 0x0137, 0x363e, _NONE_, _NONE_, _NONE_,  
       0x0b2d, 0x281d, 0x321a, 0x0307, _NONE_, _NONE_, _NONE_, _NONE_, 
       0x082a, _NONE_, _NONE_, _NONE_, _NONE_, _NONE_, _NONE_, _NONE_,  
       0x3a0c, _NONE_, _NONE_, _NONE_, _NONE_, _NONE_, _NONE_, _NONE_, 
-      0x2b1a, _NONE_, _NONE_, _NONE_, _NONE_, _NONE_, _NONE_, _NONE_};  
-
-  uint16_t table_RH[] = {
-      0x0000, 0x0506, 0x3f19, 0x2420, 0x1532, 0x0e39, 0x090f, 0x2b33, 
+      0x2b1a, _NONE_, _NONE_, _NONE_, _NONE_, _NONE_, _NONE_, _NONE_},  
+    // mode0: release+hold, mode2: none, mode4: red2
+    { 0x0000, 0x0506, 0x3f19, 0x2420, 0x1532, 0x0e39, 0x090f, 0x2b33, 
       0x0b14, 0x0407, 0x2e1e, 0x210d, 0x3038, 0x0534, 0x3a34, 0x2412,  
       0x230f, 0x3d29, 0x382f, 0x323a, 0x1236, 0x0c10, 0x2609, 0x291a, 
       0x0101, 0x1f27, 0x1006, 0x0b0d, 0x373c, 0x012a, 0x042c, 0x0b3f,  
       0x0f13, 0x3905, 0x2d36, 0x3310, 0x2723, 0x3309, 0x2225, 0x3c1a, 
       0x3c03, 0x1d25, 0x1729, 0x0303, 0x3616, 0x223c, 0x360f, 0x2829,  
       0x3725, 0x3836, 0x1a38, 0x152b, 0x1107, 0x1e14, 0x0e12, 0x0a3e, 
-      0x0a15, 0x1e3f, 0x0f21, 0x142a, 0x1b12, 0x0f38, 0x1b0b, 0x111e};
-
-  uint16_t table_GP[] = {
-      0x0000, 0x2F16, _NONE_, _NONE_, _NONE_, _NONE_, _NONE_, _NONE_, 
+      0x0a15, 0x1e3f, 0x0f21, 0x142a, 0x1b12, 0x0f38, 0x1b0b, 0x111e},
+    // mode0: group, mode2: group??, mode4: blue2
+    { 0x0000, 0x2F16, _NONE_, _NONE_, _NONE_, _NONE_, _NONE_, _NONE_, 
       _NONE_, _NONE_, _NONE_, _NONE_, _NONE_, _NONE_, _NONE_, _NONE_, 
       _NONE_, _NONE_, _NONE_, _NONE_, _NONE_, _NONE_, _NONE_, _NONE_, 
       _NONE_, _NONE_, _NONE_, _NONE_, _NONE_, _NONE_, _NONE_, _NONE_,  
       _NONE_, _NONE_, _NONE_, _NONE_, _NONE_, _NONE_, _NONE_, _NONE_, 
       _NONE_, _NONE_, _NONE_, _NONE_, _NONE_, _NONE_, _NONE_, _NONE_,  
       _NONE_, _NONE_, _NONE_, _NONE_, _NONE_, _NONE_, _NONE_, _NONE_, 
-      _NONE_, _NONE_, _NONE_, _NONE_, _NONE_, _NONE_, _NONE_, _NONE_};  
+      _NONE_, _NONE_, _NONE_, _NONE_, _NONE_, _NONE_, _NONE_, _NONE_}};  
 
   uint16_t setCRC(uint8_t *message) {          // set calculated CRC in the given message
     // return CRC1 and CRC2 values as a 16-bit word : 00aaaaaa 00bbbbbb, a is the CRC1 value, b is the CRC2 value
-    uint8_t mod = message[1] & 0x3f;
-    uint8_t g = message[2] & 0x3f;
-    uint8_t r = message[3] & 0x3f;
-    uint8_t b = message[4] & 0x3f;
-    uint8_t ar = message[5] & 0x3f;
-    uint8_t rh = message[6] & 0x3f;
-    uint8_t gp = message[7] & 0x3f;
-    uint16_t crc = INIT ^ table_M[mod] ^ table_G[g] ^ table_R[r] ^ table_B[b] ^ table_AR[ar] ^ table_RH[rh] ^ table_GP[gp];
-    
-    // add a warning if one of the values if 0xffff
+    uint16_t crc = INIT;
+    for(byte i=0;i<7;i++){
+      uint8_t value = (message[(i+1)]) & 0x3f;
+      crc ^= table[i][value];
+    }
     message[0] = (crc>>8) & 0x3f;
     message[8] = crc & 0x3f;
     return crc;
@@ -365,35 +354,25 @@ uint8_t byteArray[4][9] = {
         showLineCode();
         index=0x0;
         crcTest=1;
-        Serial.print("1 mode   (0-3f)");
-         byteArray[2][1] = ((uint8_t) readCliHex() ) & 0x3f;
-        Serial.print("2 green  (0-3f)");
-         byteArray[2][2] = ((uint8_t) readCliHex() ) & 0x3f;
-        Serial.print("3 red    (0-3f)");
-         byteArray[2][3] = ((uint8_t) readCliHex() ) & 0x3f; 
-        Serial.print("4 blue   (0-3f)");
-         byteArray[2][4] = ((uint8_t) readCliHex() ) & 0x3f; 
-        Serial.print("5 attack (0-7)");
-         byteArray[2][5] = (((uint8_t) readCliHex() ) & 0x7) << 3; 
-        Serial.print("5 random (0-7)");
-         byteArray[2][5] += ((uint8_t) readCliHex() ) & 0x7; 
-        Serial.print("6 release(0-7)");
-         byteArray[2][6] = (((uint8_t) readCliHex() ) & 0x7) << 3; 
-        Serial.print("6 hold   (0-7)");
-         byteArray[2][6] += ((uint8_t) readCliHex() ) & 0x7; 
-        Serial.print("7 Group  (0-3f)");
-         byteArray[2][7] = ((uint8_t) readCliHex() ) & 0x3f; 
-         repeatSet = (byteArray[2][5] & 0x7) + (byteArray[2][5] >> 4);  //high random and slow attack --> slow testing
-
+        Serial.print("1 mode   (0-3f)"); byteArray[2][1] =  ((uint8_t) readCliHex() ) & 0x3f;
+        Serial.print("2 green  (0-3f)"); byteArray[2][2] =  ((uint8_t) readCliHex() ) & 0x3f;
+        Serial.print("3 red    (0-3f)"); byteArray[2][3] =  ((uint8_t) readCliHex() ) & 0x3f; 
+        Serial.print("4 blue   (0-3f)"); byteArray[2][4] =  ((uint8_t) readCliHex() ) & 0x3f; 
+        Serial.print("5 attack (0-7)");  byteArray[2][5] = (((uint8_t) readCliHex() ) & 0x7) << 3; 
+        Serial.print("5 random (0-7)");  byteArray[2][5] += ((uint8_t) readCliHex() ) & 0x7; 
+        Serial.print("6 release(0-7)");  byteArray[2][6] = (((uint8_t) readCliHex() ) & 0x7) << 3; 
+        Serial.print("6 hold   (0-7)");  byteArray[2][6] += ((uint8_t) readCliHex() ) & 0x7; 
+        Serial.print("7 Group  (0-3f)"); byteArray[2][7] =  ((uint8_t) readCliHex() ) & 0x3f; 
+        repeatSet = (byteArray[2][5] & 0x7) + (byteArray[2][5] >> 4);  //high random and slow attack --> slow testing
    }
   void plainInput(){
-        Serial.print("input mode   (0-3f)"); byteArray[2][1] = (uint8_t) readCliHex();
-        Serial.print("input green  (0-3f)"); byteArray[2][2] = (uint8_t) readCliHex();
-        Serial.print("input red    (0-3f)"); byteArray[2][3] = (uint8_t) readCliHex();
-        Serial.print("input blue   (0-3f)"); byteArray[2][4] = (uint8_t) readCliHex();
-        Serial.print("input rise   (0-3f)"); byteArray[2][5] = (uint8_t) readCliHex();
-        Serial.print("input fall   (0-3f)"); byteArray[2][6] = (uint8_t) readCliHex();
-        Serial.print("input group  (0-3f)"); byteArray[2][7] = (uint8_t) readCliHex();
+        Serial.print("input mode   (0-3f)"); byteArray[2][1] = ((uint8_t) readCliHex()) & 0x3f;
+        Serial.print("input green  (0-3f)"); byteArray[2][2] = ((uint8_t) readCliHex()) & 0x3f;
+        Serial.print("input red    (0-3f)"); byteArray[2][3] = ((uint8_t) readCliHex()) & 0x3f;
+        Serial.print("input blue   (0-3f)"); byteArray[2][4] = ((uint8_t) readCliHex()) & 0x3f;
+        Serial.print("input rise   (0-3f)"); byteArray[2][5] = ((uint8_t) readCliHex()) & 0x3f;
+        Serial.print("input fall   (0-3f)"); byteArray[2][6] = ((uint8_t) readCliHex()) & 0x3f;
+        Serial.print("input group  (0-3f)"); byteArray[2][7] = ((uint8_t) readCliHex()) & 0x3f;
     }
   void commandLineInterface() {                                     // minimalistic command line interface
     char selection = readCliByte();
@@ -499,7 +478,7 @@ uint8_t byteArray[4][9] = {
         Serial.println(" a - send raw 6b8b frame");
         Serial.println(" b - send raw plain frame");
         Serial.println(" c - send values + brute the CRC");
-        Serial.println(" D - send values + CRC from table");
+        Serial.println(" d - send values + CRC from table");
         Serial.println(" w - response test - hex-string");
         Serial.println(" ? - this help");
         Serial.println(SEPARATOR);
