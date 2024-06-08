6a5a2c5aa021a989e767440193a6a1e8b60499ae
Mathias <58486836+sueppchen@users.noreply.github.com>
2024-06-08 21:55:47 +0200

    Update pixmob_cement.h 0.2.1
    clean up, add region

--- Pixmob_cement/pixmob_cement.h
+++ Pixmob_cement/pixmob_cement.h
@@ -12,12 +12,13 @@
 #include "Arduino.h"
 #include <ELECHOUSE_CC1101_SRC_DRV.h>
 
+#define EU          868.41                       /// = Xtal * 35 
+#define US          915.33                       /// = Xtal * 35 
 #define START_DELAY    800                       ///< wait until HF is present ... value in us
 #define BIT_TIME       500                       ///< inter bit delay ... us
 #define PREAMBLE      0x55                       ///< preamble ... send twice
 #define SYNC1            0                       ///< resync after preamble 
 #define SYNC2            1                       ///< resync after preamble
-#define TX_FREQ     868.49                       ///< transmit frequency for PixMob device in EU
 
 #define POLYR 0x8f3                              ///< reversed CRC12 polynom from 0xCF1     
 #define INITR 0xc69                              ///< reversed CRC12 initial value from 0x963
@@ -56,6 +57,7 @@
 class Pixmob{
   public:
     Pixmob();
+    bool begin(int pin, float frequency);
     bool begin(int pin);
     void refresh();
     void setConfirmColor(uint8_t red, uint8_t green, uint8_t blue);
@@ -74,7 +76,7 @@ class Pixmob{
     void playMemForever(uint8_t from, uint8_t to, uint8_t mRandom, uint8_t group);
     void setMasterAHR(uint8_t masterAHR, uint8_t group);
     void setMasterRelease(uint8_t masterRelease, uint8_t group);
-    void resetMasterRelease(uint8_t group);
+    void resetMasterTiming(uint8_t group);
 
     void flashDual(uint8_t red1, uint8_t green1, uint8_t blue1, uint8_t red2, uint8_t green2, uint8_t blue2);
     void flashDualLong(uint8_t red1, uint8_t green1, uint8_t blue1, uint8_t red2, uint8_t green2, uint8_t blue2);
@@ -90,7 +92,7 @@ class Pixmob{
     void playMemForever(uint8_t from, uint8_t to, uint8_t mRandom);
     void setMasterAHR(uint8_t masterAHR);
     void setMasterRelease(uint8_t masterRelease);
-    void resetMasterRelease();
+    void resetMasterTiming();
   private:
     const uint8_t dictTable[64] = {
         0x21, 0x35, 0x2c, 0x34, 0x66, 0x26, 0xac, 0x24, 0x46, 0x56, 0x44, 0x54, 0x64, 0x6d, 0x4c, 0x6c,     ///< 6b-8b conversion table for 0x00 - 0x0f
